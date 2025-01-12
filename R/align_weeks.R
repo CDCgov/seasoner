52f74a780cd7a3cd3cb057b94ff8c6611d62de10
Scott Olesen <ulp7@cdc.gov>
2025-01-12 12:10:39 -0500

    checkpoint: validate lengths

--- R/align_weeks.R
+++ R/align_weeks.R
@@ -58,7 +58,7 @@ align_weeks <- function(
 #' @return integer seasons (indicating first year of two-year season)
 date_to_season <- function(x, start_month, start_day) {
   before_season_start <- (
-    lubridate::month(x) < start_month |
+    (lubridate::month(x) < start_month) |
       ((lubridate::month(x) == start_month) & (lubridate::day(x) < start_day))
   )
   # if before season start, then you belong to the season
@@ -67,6 +67,56 @@ date_to_season <- function(x, start_month, start_day) {
   lubridate::year(x) - before_season_start
 }
 
-get_ref_date <- function(season, start_day, start_month) {
+#' Get the reference date in a season
+#'
+#' @param season First year of two-year season
+#' @param season_start_month Integer month of season start (1=January)
+#' @param season_start_day Calendar day of season start
+#' @param ref_month Integer month of reference dates
+#' @param ref_day Calendar day of reference dates
+#'
+#' @return vector of dates
+ref_date_in_season <- function(season, season_start_month, season_start_day, ref_month, ref_day) {
+  before_season_start <- (
+    (ref_month < season_start_month) |
+      ((ref_month == season_start_month) & (ref_day < season_start_day))
+  )
 
+  ISOdate(season + before_season_start, ref_month, ref_day)
+}
+
+#' Validate argument lengths
+#'
+#' All arguments must be of the same length, or they call all be of
+#' length 1 except for a single argument
+#'
+#' @param ... named or positional arguments
+#'
+#' @return `TRUE`, or throw error
+validate_lengths <- function(...) {
+  # named list of the arguments
+  args <- list(...)
+  # number of arguments
+  n_args <- length(args)
+  # vector of lengths of each argument, potentially named
+  lens <- sapply(args, length)
+  # number of unique argument lengths
+  n_unique_lens <- length(unique(lens))
+  # number of arguments of length 1
+  n_len_1 <- sum(lens == 1)
+  # vector of number of times each argument-length occurs
+  tab <- tabulate(lens)
+
+  # otherwise, check that all arguments except one are of
+  # length 1
+  if (n_unique_lens == 1) {
+    # if all arguments are the same length, pass!
+    return(TRUE)
+  } else if (n_unique_lens == 2 && n_args == n_len_1 + 1) {
+    # if there are two unique lengths, and only one argument
+    # is not of length 1, also pass!
+    return(TRUE)
+  } else {
+    stop(paste0(c("Mismatched argument lengths:", lens, collapse = " ")))
+  }
 }
---
+++ tests/testthat/test_ref_date_in_season.R
@@ -0,0 +1,28 @@
+library(testthat)
+
+test_that("After season start gives year = season", {
+  expect_equal(
+    ref_date_in_season(season = 2020, season_start_month = 7, season_start_day = 1, ref_month = 8, ref_day = 1),
+    ISOdate(2020, 8, 1)
+  )
+})
+
+test_that("Before season start gives year = season + 1", {
+  expect_equal(
+    ref_date_in_season(season = 2020, season_start_month = 7, season_start_day = 1, ref_month = 2, ref_day = 1),
+    ISOdate(2021, 2, 1)
+  )
+})
+
+test_that("Vectorized works", {
+  expect_equal(
+    ref_date_in_season(
+      season = c(2020, 2021, 2022),
+      season_start_month = 7,
+      season_start_day = 1,
+      ref_month = 2,
+      ref_day = 1
+    ),
+    ISOdate(c(2021, 2022, 2023), 2, 1)
+  )
+})
