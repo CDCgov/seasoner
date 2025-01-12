fb03d6a1e76ea3f6a0bc483e78a52c2853273c70
Scott Olesen <ulp7@cdc.gov>
2025-01-12 16:29:45 -0500

    checkpoint: writing tests

--- tests/testthat/test_align_weeks.R
+++ tests/testthat/test_align_weeks.R
@@ -8,3 +8,12 @@ test_that("Bad days of the week error", {
     week_day = 6
   ))
 })
+
+test_that("Weeks in the same season do not move", {
+  # Aug 1, 2020 and Feb 1, 2021 are both in the 2020/2021 season
+  x <- as.Date(c(ISOdate(2020, 8, 1), ISOdate(2021, 2, 1)))
+  expect_equal(
+    align_weeks(x, target_season = 2020, week_day = 6),
+    x
+  )
+})
